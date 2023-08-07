bb63382d030f46e5c0379676e79fa0cad97e29c0
etyumemntcev <etyumentacve@gmail.com>
2023-08-07 22:11:22 +0600

    Определен интерефейс ICommand - реализация паттерна Команда. Установлена версия пакета 0.1.0.0

---
+++ appserver/core/ICommand.cs
@@ -0,0 +1,16 @@
+﻿namespace AppServer
+{
+    /// <summary>
+    /// Команда.
+    /// </summary>
+    public interface ICommand
+    {
+        /// <summary>
+        /// Выполнить Команду.
+        /// 
+        /// Если Команда по какой-либо причине не может быть выполнена, 
+        /// то выбрасывается исключение <see cref="System.Exception"/>
+        /// </summary>
+        void Execute();
+    }
+}
