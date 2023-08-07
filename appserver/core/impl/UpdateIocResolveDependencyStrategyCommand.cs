06491b4f13201dd9604a1243ab2e996ee043b5db
etyumemntcev <etyumentacve@gmail.com>
2023-08-08 02:29:42 +0600

    Перенесена реализация по-умолчанию Ioc в пространство имен AppServer.Impl

---
+++ appserver/core/impl/UpdateIocResolveDependencyStrategyCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace AppServer.Impl
+{
+    internal class UpdateIocResolveDependencyStrategyCommand : ICommand
+    {
+        Func<Func<string, object[], object>, Func<string, object[], object>> _updater;
+
+        public UpdateIocResolveDependencyStrategyCommand(
+            Func<Func<string, object[], object>, Func<string, object[], object>> updater
+        ) => _updater = updater;
+
+        public void Execute()
+        {
+            Ioc._strategy = _updater(Ioc._strategy);
+        }
+    }
+}
