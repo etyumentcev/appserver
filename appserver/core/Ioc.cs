09a5d2b40287f4d7f18cf80836b5506ffc7cf09e
etyumemntcev <etyumentacve@gmail.com>
2023-08-08 01:05:38 +0600

    Первоначальное определение класса Ioc

---
+++ appserver/core/Ioc.cs
@@ -0,0 +1,10 @@
+﻿namespace AppServer
+{
+    public class Ioc
+    {
+        public static T Resolve<T>(string dependency, params object[] args)
+        {
+            return (T) new object();
+        }
+    }
+}
