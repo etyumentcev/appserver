d5ab7e531bb7eb520f9e8fc52084add853e975a9
Eugeniy Tyumentcev <etyumentcev@gmail.com>
2023-08-08 11:45:50 +0600

    Ioc definition (#2)
    1. Первоначальное определение класса Ioc:
    
    * Ioc позволяет переопределять стратегию разрешения зависимостей
    
    * Если зависимость не найдена, то Ioc выбрасывает ArgumentException
    
    * Ioc должен выбрасывать исключение InvalidCastException, если разрешенный объект имеет тип, который не приводится к указанному в качестве параметра Generic
    
    * Документация Ioc
    
    1. Сборка тестов в Debug режиме
    1. Миграция на xUnit
    1. Добавление в проект пакета coverlet.msbuild для использования coverage с msbuild
    1. Отчет о покрытии кода тестами
    
    ---------
    
    Co-authored-by: etyumemntcev <etyumentacve@gmail.com>

---
+++ appserver/core.tests/IocTests.cs
@@ -0,0 +1,42 @@
+namespace AppServer
+{
+    public class IocTests
+    {
+        [Fact]
+        public void IocShouldUpdateResolveDependencyStrategy()
+        {
+            bool wasCalled = false;
+
+            Ioc.Resolve<ICommand>(
+                "Update Ioc Resolve Dependency Strategy",
+                (Func<string, object[], object> args) => 
+                {
+                    wasCalled = true;
+                    return args;
+                }
+            ).Execute();
+
+            Assert.True( wasCalled );
+        }
+
+        [Fact]
+        public void IocShouldThrowArgumentExceptionIfDependencyIsNotFound()
+        {
+            Assert.Throws<ArgumentException>(
+                () => Ioc.Resolve<object>("UnexistingDependency")
+            );
+        }
+
+        [Fact]
+        public void IocShouldThrowInvalidCastExceptionIfDependencyResolvesAnotherType()
+        {
+            Assert.Throws<InvalidCastException>(
+              () => Ioc.Resolve<string>(
+                "Update Ioc Resolve Dependency Strategy",
+                (Func<string, object[], object> args) => args
+              )
+            );
+        }
+
+    }
+}
---
+++ appserver/core/impl/UpdateIocResolveDependencyStrategyCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace AppServer.Impl
+{
+    internal class UpdateIocResolveDependencyStrategyCommand : ICommand
+    {
+        Func<Func<string, object[], object>, Func<string, object[], object>> _updateIoCStrategy;
+
+        public UpdateIocResolveDependencyStrategyCommand(
+            Func<Func<string, object[], object>, Func<string, object[], object>> updater
+        ) => _updateIoCStrategy = updater;
+
+        public void Execute()
+        {
+            Ioc._strategy = _updateIoCStrategy(Ioc._strategy);
+        }
+    }
+}
