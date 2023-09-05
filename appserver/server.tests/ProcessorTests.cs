a4ce3a657946953584dce070e70b95b7e1fd3d76
Eugeniy Tyumentcev <etyumentcev@gmail.com>
2023-09-05 11:18:38 +0600

    Command processor (#7)
    * Выполнение команд в отдельном потоке.
    
    ---------
    
    Co-authored-by: etyumemntcev <etyumentacve@gmail.com>

--- appserver/server.tests/ProcessorTests.cs
+++ appserver/server.tests/ProcessorTests.cs
@@ -1,10 +1,16 @@
 using Moq;
+using AppServer.Scopes;
 using System.Collections.Concurrent;
 
 namespace AppServer.Commands.Tests
 {
     public class ProcessorTests
     {
+        public ProcessorTests()
+        {
+            new Scopes.InitCommand().Execute();
+        }
+
         [Fact]
         public void HardStopCommand_Should_Stop_Processor_Immediately()
         {
@@ -60,6 +66,48 @@ namespace AppServer.Commands.Tests
             Assert.True(processor.Wait(5000));
             Assert.True(processorContext.ContainsKey("terminateException"));
             cmd.VerifyAll();
+        }
+
+        [Fact]
+        public void Processor_Can_Be_Continued_If_Processable_Can_Handle_Exception()
+        {
+            Dictionary<string, object> processorContext = new Dictionary<string, object>();
+            new InitProcessorContextCommand(processorContext).Execute();
+
+            bool exceptionWasHandled = false;
+
+            var queue = (BlockingCollection<ICommand>)processorContext["queue"];
+            processorContext["exceptionHandler"] = (ICommand cmd, Exception ex) =>
+            {
+                exceptionWasHandled = true;
+            };
+
+            var cmd = new Mock<ICommand>();
+            cmd.Setup(x => x.Execute()).Throws<Exception>().Verifiable();
+            queue.Add(cmd.Object);
+            queue.Add(new HardStopCommand(processorContext));
+
+            var processor = new Processor(new Processable(processorContext));
+
+            Assert.True(processor.Wait(5000));
+            Assert.True(exceptionWasHandled);
+            cmd.VerifyAll();
+        }
+
+        [Fact]
+        public void Prcocessor_Can_Be_Created_By_Server_CommandProcessor_Dependency()
+        {
+            new RegisterProcessorCommand().Execute();
+
+            var processorContext = Ioc.Resolve<IDictionary<string, object>>("Server.CommandProcessor");
+            var processor = (Processor)processorContext["processor"];
+
+            var queue = (BlockingCollection<ICommand>)processorContext["queue"];
+
+            queue.Add(new HardStopCommand(processorContext));
+
+            Assert.True(processor.Wait(5000));
+
         }
     }
 }
