3d9e77348954c8a7215d6661a1efb932c97c97f5
Eugeniy Tyumentcev <etyumentcev@gmail.com>
2024-01-05 16:38:07 +0600

    Реализована команда Repeat (#12)
    Co-authored-by: etyumemntcev <etyumentacve@gmail.com>

---
+++ appserver/commands/SendCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace AppServer.Commands
+{
+    public class SendCommand : ICommand
+    {
+        ICommand _repeatableCommand;
+        ICommandConsumer _receiver;
+
+        public SendCommand(ICommand cmd, ICommandConsumer receiver)
+        {
+            _repeatableCommand = cmd;
+            _receiver = receiver;
+        }
+
+        public void Execute()
+        {
+            _receiver.Receive(_repeatableCommand);
+        }
+    }
+}
