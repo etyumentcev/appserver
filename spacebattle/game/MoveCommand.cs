1efd4c21e616a45aa47419c44ec34eb0fa168ca0
etyumemntcev <etyumentacve@gmail.com>
2024-01-05 11:49:47 +0600

    Реализована команда прямолинейного равномерного движения без деформации

---
+++ spacebattle/game/MoveCommand.cs
@@ -0,0 +1,15 @@
+﻿using AppServer;
+
+namespace SpaceBattle
+{
+    public class MoveCommand : ICommand
+    {
+        IMovable _movable;
+        public MoveCommand(IMovable movable) => _movable = movable;
+
+        public void Execute() 
+        {
+            _movable.Location += _movable.Velocity;
+        }
+    }
+}
